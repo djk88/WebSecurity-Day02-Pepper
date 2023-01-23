e6d41e779699b6f7b999a20f8162cbf05ab58d05
djk88 <dkim104@my.bcit.ca>
2023-01-23 14:07:38 -0800

    Add project files.

---
+++ WebSecurity-Day02-Pepper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSecurity_Day02_Pepper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
